50af760d42f99769647873e9b2109ee4db9445e9
Battarey <killer.tim2018@gmail.com>
2025-04-07 16:05:30 +0300

    first commit

---
+++ functions.py
@@ -0,0 +1,19 @@
+import time as t # Fo use method sleep()
+import os # To clean console
+from workWithDB import check_user_credentials
+
+def clearConsole():
+    os.system('cls')
+
+def enterLoginAndPassword():
+    login = input('Input your login: ')
+    password = input('Input your password: ')
+    if accountVerification(login, password):
+        print(f"Successful authorization! Welcome, {login}!"), t.sleep(4)
+    else:
+        print("Incorrect login or password! Try again!"), t.sleep(5), enterLoginAndPassword()
+
+def accountVerification(login, password):
+    return check_user_credentials(login, password)
+
+
---
+++ workWithDB.py
@@ -0,0 +1,53 @@
+# PostgreSQL, Designed to store and work with user accounts
+import psycopg2
+from psycopg2 import OperationalError, DatabaseError
+from config import USERS_DB_CONFIG
+
+connection = None
+cursor = None
+
+def connect_to_db():
+    global connection, cursor
+    
+    try:
+        connection = psycopg2.connect(**USERS_DB_CONFIG)
+        cursor = connection.cursor()
+        return True
+    except OperationalError as e:
+        print(f"Connection error to DataBase: {e}")
+        return False
+def close_db_connection():
+    global connection, cursor
+    
+    if cursor:
+        cursor.close()
+    
+    if connection:
+        connection.close()
+    
+    connection = None
+    cursor = None
+
+def check_user_credentials(login, password):
+    connect_to_db()
+    if not connection or connection.closed:
+        print("There is no active connection to the database")
+        return False
+    
+    try:
+        query = """
+        SELECT EXISTS(
+            SELECT 1 FROM users 
+            WHERE users_login = %s AND users_password = %s
+        )
+        """
+        cursor.execute(query, (login, password))
+        return cursor.fetchone()[0]
+    except DatabaseError as e:
+        print(f"Error while checking data: {e}")
+        return False
+    finally:
+        close_db_connection()
+
+# MongoDB, stores user files
+
